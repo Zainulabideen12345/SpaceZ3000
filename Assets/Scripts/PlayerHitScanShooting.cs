293e96c260ade0e608c410bd27259bfa0d9fa84a
PeterPwlk <piotr.pawlak101@gmail.com>
2020-03-18 00:00:30 +0100

    Implemented basic raycast shooting.

---
+++ Assets/Scripts/PlayerHitScanShooting.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class PlayerHitScanShooting : MonoBehaviour
+    {
+        private Camera _camera;
+        [SerializeField] private ShootingPoint[] shootingPoints;
+
+        private void Start()
+        {
+            _camera = Camera.main;
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var mouseWorldPoint = _camera.ScreenToWorldPoint(Input.mousePosition);
+                foreach (var shootingPoint in shootingPoints.Where(point => point.IsActive))
+                {
+                    ShootFromShootPoint(mouseWorldPoint, shootingPoint);
+                }
+            }
+        }
+
+        private static void ShootFromShootPoint(Vector3 mouseWorldPoint, ShootingPoint shootingPoint)
+        {
+            var gunEndPoint = shootingPoint.GunEndPoint;
+            var shootPoint = (mouseWorldPoint - gunEndPoint).normalized;
+            Debug.DrawLine(gunEndPoint, mouseWorldPoint, Color.red, .1f);
+            ShootingRaycast.Shoot(shootingPoint.GunEndPoint, shootPoint);
+        }
+    }
+}
---
+++ Assets/Scripts/ShootingPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class ShootingPoint : MonoBehaviour
+    {
+        [SerializeField] private bool isActive = false;
+
+        public Vector3 GunEndPoint => transform.position;
+
+        public bool IsActive => isActive;
+        
+    }
+}
