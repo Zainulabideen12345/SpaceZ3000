0b4db79c85200807db17754a76deb99e016e3634
Marcin Piskorz <marcinpiskorz01@gmail.com>
2020-04-15 00:06:05 +0200

    Enemy spawners added

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private GameObject spawnAnimationPrefab;
+    [SerializeField] private float delay;
+
+    private GameObject _enemy;
+    private GameObject _spawnAnimation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_enemy == null && _spawnAnimation == null)
+        {
+            SpawnEnemy();
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        SpawnAnimationPrefab();
+        Invoke(nameof(SpawnEnemyPrefab), delay);
+    }
+
+    private void SpawnAnimationPrefab()
+    {
+        _spawnAnimation = Instantiate(spawnAnimationPrefab, transform.position, transform.rotation) as GameObject;
+    }
+
+    private void SpawnEnemyPrefab()
+    {
+        Destroy(_spawnAnimation);
+        _enemy = Instantiate(enemyPrefab, transform.position, transform.rotation) as GameObject;
+    }
+}
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using Pathfinding;
 
 public class Enemy : MonoBehaviour
 {
@@ -9,15 +10,18 @@ public class Enemy : MonoBehaviour
     private const int SPRITE_ANGLE_DIFF = 270;
 
     private Rigidbody2D _rb;
-    private Transform _player;
+    private GameObject _player;
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _player = GameObject.Find("Player").transform;
+        _player = GameObject.Find("Player");
 
-        InvokeRepeating("Shoot", .5f, 1f / attackSpeed);
+        gameObject.AddComponent(typeof (AIDestinationSetter));
+        GetComponent<AIDestinationSetter>().target = _player.transform;
+
+        InvokeRepeating(nameof(Shoot), .5f, 1f / attackSpeed);
     }
 
     // Update is called once per frame
@@ -27,9 +31,9 @@ public class Enemy : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (Vector2.Distance(_rb.transform.position, _player.position) < 3f)
+        if (Vector2.Distance(_rb.transform.position, _player.transform.position) < 3f)
         {
-            Vector3 direction = _player.position - transform.position;
+            Vector3 direction = _player.transform.position - transform.position;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + SPRITE_ANGLE_DIFF;
 
             _rb.rotation = angle;
