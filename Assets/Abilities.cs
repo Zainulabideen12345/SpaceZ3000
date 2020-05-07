145e8a1dc34f58dabedf8dc5491eff78e57f8d37
Castelias <Ninja@julita.pl>
2020-05-07 16:23:30 +0200

    Added player abilities

---
+++ Assets/Abilities.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Abilities : MonoBehaviour
+{
+    public Transform ShootingPoint_Mid;
+    public GameObject missilePrefab;
+    private PlayerInput _playerInput;
+    public GameObject haloPrefab;
+    
+
+
+    private void Awake()
+    {
+
+        _playerInput = new PlayerInput();
+        _playerInput.PlayerControls.UseUltimate.performed += ctx => StartCoroutine(UltimateMissile());
+        _playerInput.PlayerControls.UseHalo.performed += ctx => UltimateHalo();
+
+    }
+
+
+    private void OnEnable()
+    {
+        _playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerInput.Disable();
+    }
+    
+
+
+
+    private IEnumerator UltimateMissile()
+    {
+        for (int i = 0; i <= 9; i++)
+        {
+            GameObject Ulti = Instantiate(missilePrefab, ShootingPoint_Mid.position, ShootingPoint_Mid.rotation);
+
+            yield return new WaitForSeconds(1);
+        }
+
+    }
+    void UltimateHalo()
+    {
+        GameObject Ulti2 = Instantiate(haloPrefab, ShootingPoint_Mid.position, ShootingPoint_Mid.rotation);
+    }
+
+
+
+
+
+
+
+
+}
---
+++ Assets/missile.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+namespace DefaultNamespace
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class missile : MonoBehaviour
+    {
+        public Transform target;
+        public float speed = 5f;
+        public float rotateSpeed = 200f;
+        private Rigidbody2D rb;
+        public int missileDamage = 100;
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            target = GameObject.FindGameObjectWithTag("Enemy").transform;
+
+        }
+        private void FixedUpdate()
+        {
+            Vector2 direction = (Vector2)target.position - rb.position;
+
+            direction.Normalize();
+
+            float rotateAmount = Vector3.Cross(direction, transform.up).z;
+
+            rb.angularVelocity = -rotateAmount * rotateSpeed;
+
+            rb.velocity = transform.up * speed;
+
+        }
+        private void OnTriggerEnter2D(Collider2D hitInfo)
+        {
+            var health = hitInfo.gameObject.GetComponent<HealthManager>();
+            health?.DealDamage(missileDamage);
+
+            Enemy enemy = hitInfo.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                Destroy(gameObject);
+
+            }
+        }
+    }
+}
+
