c7f0c4a6c8782fff29a6414d09052346288436a9
Michael Walsh <michaelwalsh223@hotmail.co.uk>
2020-06-06 20:23:03 +0100

    Fixed all causes of exceptions in Enemy class

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -17,7 +17,7 @@ public class Enemy : MonoBehaviour
     private Rigidbody2D _rb;
     private GameObject _player;
     private float _timeToDestroy = 3;
-    
+
 
     // Start is called before the first frame update
     void Start()
@@ -29,12 +29,12 @@ public class Enemy : MonoBehaviour
         GetComponent<AIDestinationSetter>().target = EnemyTargetPointsController.GetUniquePoint();
 
         InvokeRepeating(nameof(Shoot), .5f, 1f / attackSpeed);
-    } 
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(_player != null)
+        if (_player != null)
         {
             transform.up = _player.transform.position - transform.position;
         }
@@ -42,9 +42,12 @@ public class Enemy : MonoBehaviour
 
     private void OnDestroy()
     {
-        _player?.GetComponent<PlayerAbilitiesController>().AddEnergy(energyAmountAdded);
-        _player?.GetComponent<Player>().AddKills();
-
+        if(_player != null)
+        {
+            _player?.GetComponent<PlayerAbilitiesController>().AddEnergy(energyAmountAdded);
+            _player?.GetComponent<Player>().AddKills();
+        }
+        
         foreach (GameObject effect in deathEffects)
         {
             var explosion = Instantiate(effect, transform.position, transform.rotation);
@@ -54,26 +57,36 @@ public class Enemy : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (_player == null)
+        {
+            return;
+        }
+
         if (Vector2.Distance(_rb.transform.position, _player.transform.position) < 3f)
         {
             Vector3 direction = _player.transform.position - transform.position;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + SPRITE_ANGLE_DIFF;
 
             _rb.rotation = angle;
-        } 
+        }
     }
 
     private void Shoot()
     {
+        if (_player == null)
+        {
+            return;
+        }
+
         if (Vector2.Distance(gameObject.transform.position, _player.transform.position) < ShootingRange)
         {
             var bullet1 = Instantiate(bulletPrefab, transform.GetChild(0).transform.position, transform.rotation);
             var bullet2 = Instantiate(bulletPrefab, transform.GetChild(1).transform.position, transform.rotation);
-            
+
             bullet1.GetComponent<Projectile>().SetVelocity(transform.up);
             bullet2.GetComponent<Projectile>().SetVelocity(transform.up);
         }
-            AudioManager.instance.Play("Pew");
+
+        AudioManager.instance.Play("Pew");
     }
-    
 }
