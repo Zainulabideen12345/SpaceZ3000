2014ebf9f7c7afdc9d2ea6d7f7dbcc34521576a6
Marcin Piskorz <marcinpiskorz01@gmail.com>
2020-06-06 13:30:45 +0200

    #72 Minimap added to the UI

---
+++ Assets/Minimap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    private GameObject _player;
+
+    private void LateUpdate()
+    {
+        _player = GameObject.Find("Player");
+
+        Vector3 newPosition = _player.transform.position;
+        newPosition.z = transform.position.z;
+        transform.position = newPosition;
+    }
+}
