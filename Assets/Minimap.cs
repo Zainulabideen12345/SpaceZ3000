7a9a80687278adab4053b0cb0015336b49f5d932
Marcin Piskorz <marcinpiskorz01@gmail.com>
2020-06-06 15:46:57 +0200

    #72 Minimap added to the UI (#92)
    * #72 Minimap added to the UI
    
    * #72 Minimap zoomed out

---
+++ Assets/Minimap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    private GameObject _player;
+
+    private void Start()
+    {
+        _player = GameObject.Find("Player");
+    }
+    private void LateUpdate()
+    {
+        if (_player != null)
+        {
+            Vector3 newPosition = _player.transform.position;
+            newPosition.z = transform.position.z;
+            transform.position = newPosition;
+        }
+    }
+}
